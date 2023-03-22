9651adb462db6c050f85adb28ba16a7a4374aec2
valeriofilareti <valerio.filareti@gmail.com>
2023-03-22 19:09:36 +0100

    [add] btn event listener + output

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 2) creare un primo if che stampi fizzbuzz sui multipli di 3 e 5 attraverso '&&'
 3) creare una seconda condizione che stampi 'fizz' sui multipli di 3
 4) crearte una quarta condizione che stampi 'buzz' sui multipli di 5
+5) creare una funzione che, al click sul bottone, stampi gli output in pagina
--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 const btn = document.querySelector('button')
 
+const main = document.querySelector('main')
 
-for(i=1; i<=100; i++){
-  if(i%3 == 0 && i%5 == 0){
-  console.log(i + ' fizzbuzz')
-}else if(i%3 == 0){
-  console.log(i + ' fizz')
-}else if(i%5 == 0){
-  console.log(i + ' buzz')
-}else{
-  console.log(i)
-}
-}
+
+
+btn.addEventListener('click', function(){
+
+
+  for(i=1; i<=100; i++){
+    if(i%3 == 0 && i%5 == 0){
+    let box = document.createElement('div')
+    main.append(box)
+    box.classList.add('box')
+    box.append('fizzbuzz')
+  }else if(i%3 == 0){
+    let box = document.createElement('div')
+    main.append(box)
+    box.classList.add('box')
+    box.append('fizz')
+  }else if(i%5 == 0){
+    let box = document.createElement('div')
+    main.append(box)
+    box.classList.add('box')
+    box.append('buzz')
+  }else{
+    let box = document.createElement('div')
+    main.append(box)
+    box.classList.add('box')
+    box.append(i)
+  }
+  }
+
+
+})
+
